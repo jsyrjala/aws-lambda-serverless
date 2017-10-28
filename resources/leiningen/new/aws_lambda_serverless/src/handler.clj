6059641d6b22456d950a3d02316049b2c965e614
Juha Syrjälä <juha.syrjala@iki.fi>
2017-10-28 09:46:47 +0300

    Move lambda-handler to lambda-util.clj

--- resources/leiningen/new/aws_lambda_serverless/src/handler.clj
+++ resources/leiningen/new/aws_lambda_serverless/src/handler.clj
@@ -1,11 +1,9 @@
 (ns {{name}}.handler
   "FIXME AWS Lambda to do XXX"
-  (:require [clojure.tools.logging :as log]
-            [{{name}}.lambda-util :as lambda-util])
-  (:import [java.io InputStream OutputStream]
-           [java.util UUID])
+  (:require [{{name}}.lambda-util :as lambda-util])
+  (:import [java.io InputStream OutputStream])
   (:gen-class
-    ;; Need to define Lambda handler method signature here so that AWS can find the method
+    ;; Define handler method signature here so that AWS can find the method
     :methods [^:static [handler [java.io.InputStream java.io.OutputStream] void]])
   )
 
@@ -16,40 +14,16 @@
   (.write output (.getBytes "TODO implement me\n" "UTF-8"))
   )
 
-;;;;;;;;;;;;;;;;;;;;;;
-
-;; Counter to track how many times this process has been invoked.
-(def start-counter (atom 0))
-;; Unique id for this process
-(def instance-id (.toString (UUID/randomUUID)))
-
-(defn lambda-handler
-  "Business happens here"
-  [execute-fn ^InputStream input ^OutputStream output]
-  (try
-    (log/info "Starting {{name}}" (or (lambda-util/manifest-version) ""))
-    (swap! start-counter inc)
-    (log/info "Lambda instance" instance-id "has been invoked" @start-counter "times including current invocation.")
-
-    (lambda-execute input output)
-
-    (log/info "Lambda successful")
-    (catch Exception ex
-      (log/error "Lambda failed:" (.getMessage ex))
-      (throw ex))
-    (finally
-      (log/info "Lambda finishing."))) )
-
 (defn -handler
   "AWS Lambda entry point"
   [^InputStream input ^OutputStream output]
-  (lambda-handler lambda-execute input output))
+  (lambda-util/lambda-handler lambda-execute input output))
 
 (defn -main
   "Java main method. For testing. Reads stdin.
     e.g java -jar lambda.jar < file"
   []
   (try
-    (lambda-handler lambda-execute System/in System/out)
+    (lambda-util/lambda-handler lambda-execute System/in System/out)
     (finally
       (shutdown-agents))))
--- resources/leiningen/new/aws_lambda_serverless/src/lambda_util.clj
+++ resources/leiningen/new/aws_lambda_serverless/src/lambda_util.clj
@@ -1,5 +1,10 @@
 (ns {{name}}.lambda-util
-  (:import [java.util.jar Manifest]))
+  (:require [clojure.tools.logging :as log]
+    )
+  (:import [java.io InputStream OutputStream]
+           [java.util UUID]
+           [java.util.jar Manifest])
+  )
 
 (defn manifest-version
   "Fetch project version from META-INF/MANIFEST.MF. Works only on uberjar."
@@ -11,3 +16,25 @@
         attributes (when manifest (.getMainAttributes manifest))]
     (when attributes
       (.getValue attributes "Implementation-Version"))))
+
+;; Counter to track how many times this process has been invoked.
+(def start-counter (atom 0))
+;; Unique id for this process
+(def instance-id (.toString (UUID/randomUUID)))
+
+(defn lambda-handler
+  "Business happens here"
+  [execute-fn ^InputStream input ^OutputStream output]
+  (try
+    (log/info "Starting {{name}}" (or (manifest-version) ""))
+    (swap! start-counter inc)
+    (log/info "Lambda instance" instance-id "has been invoked" @start-counter "times including current invocation.")
+
+    (execute-fn input output)
+
+    (log/info "Lambda successful")
+    (catch Exception ex
+      (log/error "Lambda failed:" (.getMessage ex))
+      (throw ex))
+    (finally
+      (log/info "Lambda finishing."))) )
