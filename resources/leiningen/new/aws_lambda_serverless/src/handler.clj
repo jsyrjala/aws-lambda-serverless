1dfc57f7faa6c00d5ebcb25897bd07f3f0b88ea1
Juha Syrjälä <juha.syrjala@iki.fi>
2017-10-29 14:21:06 +0200

    Support multiple lambda functions in single uberjar.

--- resources/leiningen/new/aws_lambda_serverless/src/handler.clj
+++ resources/leiningen/new/aws_lambda_serverless/src/handler.clj
@@ -4,26 +4,37 @@
   (:import [java.io InputStream OutputStream])
   (:gen-class
     ;; Define handler method signature here so that AWS can find the method
-    :methods [^:static [handler [java.io.InputStream java.io.OutputStream] void]])
+    :methods [^:static [func1 [java.io.InputStream java.io.OutputStream] void]
+              ^:static [func2 [java.io.InputStream java.io.OutputStream] void]])
   )
 
-(defn lambda-execute
-  "FIXME Business happens here"
+(defn func1-execute
+  "FIXME func1 does XXX"
   [^InputStream input ^OutputStream output]
   ;; FIXME Add lambda implementation here
-  (.write output (.getBytes "TODO implement me\n" "UTF-8"))
+  (.write output (.getBytes "TODO implement func1\n" "UTF-8"))
   )
 
-(defn -handler
+(defn func2-execute
+  "FIXME func2 does XXX"
+  [^InputStream input ^OutputStream output]
+  ;; FIXME Add lambda implementation here
+  (.write output (.getBytes "TODO implement func2\n" "UTF-8"))
+  )
+
+(defn -func1
   "AWS Lambda entry point"
   [^InputStream input ^OutputStream output]
-  (lambda-util/lambda-handler lambda-execute input output))
+  (lambda-util/lambda-handler func1-execute input output))
+
+(defn -func2
+  "AWS Lambda entry point"
+  [^InputStream input ^OutputStream output]
+  (lambda-util/lambda-handler func2-execute input output))
 
 (defn -main
   "Java main method. For testing. Reads stdin.
     e.g java -jar lambda.jar < file"
-  []
-  (try
-    (lambda-util/lambda-handler lambda-execute System/in System/out)
-    (finally
-      (shutdown-agents))))
+  [& args]
+  (lambda-util/main-handler {:func1 func1-execute
+                             :func2 func2-execute} args))
