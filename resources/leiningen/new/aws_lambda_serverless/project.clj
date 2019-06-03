9ccbc13e0e232cb41940257cb619331c22123f5e
Juha Syrjälä <juha.syrjala@iki.fi>
2019-06-03 22:44:35 +0300

    slf4j 1.7.26

--- resources/leiningen/new/aws_lambda_serverless/project.clj
+++ resources/leiningen/new/aws_lambda_serverless/project.clj
@@ -6,9 +6,9 @@
 
                  [org.clojure/tools.logging "0.4.1"]
                  [ch.qos.logback/logback-classic "1.2.3"]
-                 [org.slf4j/slf4j-api "1.7.25"]
-                 [org.slf4j/log4j-over-slf4j "1.7.25"]
-                 [org.slf4j/jcl-over-slf4j "1.7.25"]
+                 [org.slf4j/slf4j-api "1.7.26"]
+                 [org.slf4j/log4j-over-slf4j "1.7.26"]
+                 [org.slf4j/jcl-over-slf4j "1.7.26"]
                  ]
   :jvm-opts ["-Dclojure.compiler.elide-meta=[:doc]"
              "-Dclojure.compiler.direct-linking=true"]
