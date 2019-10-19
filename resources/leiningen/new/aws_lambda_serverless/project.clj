7483a64552e0a31b5e951b18dbd84aacd642ea16
Juha Syrjälä <juha.syrjala@iki.fi>
2019-10-19 16:23:58 +0300

    Update library versions in generated project.clj

--- resources/leiningen/new/aws_lambda_serverless/project.clj
+++ resources/leiningen/new/aws_lambda_serverless/project.clj
@@ -2,13 +2,13 @@
   :description "AWS Lambda to do FIXME"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
 
-                 [org.clojure/tools.logging "0.4.1"]
+                 [org.clojure/tools.logging "0.5.0"]
                  [ch.qos.logback/logback-classic "1.2.3"]
-                 [org.slf4j/slf4j-api "1.7.26"]
-                 [org.slf4j/log4j-over-slf4j "1.7.26"]
-                 [org.slf4j/jcl-over-slf4j "1.7.26"]
+                 [org.slf4j/slf4j-api "1.7.28"]
+                 [org.slf4j/log4j-over-slf4j "1.7.28"]
+                 [org.slf4j/jcl-over-slf4j "1.7.28"]
                  ]
   :jvm-opts ["-Dclojure.compiler.elide-meta=[:doc]"
              "-Dclojure.compiler.direct-linking=true"]
